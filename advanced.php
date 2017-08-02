f0b844a15408c008eadba91d39c6ef622dc5ef64
Robbert Stevens <Robbert Stevens>
2017-08-02 20:54:19 +0200

    Initial Commit

---
+++ advanced.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * A more advance version of the fuzzbuz solution
+ */
+function fizzbuzz($iterations, $rules) {
+    for($i = 1; $i <= $iterations; $i++) {
+        $array = [$i];
+
+        foreach($rules as $diviser => $word) {
+            if ($i % $diviser == 0) $array[] = $word;
+        }
+        if (count($array) > 1) array_shift($array);
+
+        echo sprintf("%s <br>", implode(" ", $array));
+    }
+}
+
+
+fizzbuzz(100, [
+    3 => "three",
+    4 => "four",
+    10 => "ten",
+]);
---
+++ simple.php
@@ -0,0 +1,19 @@
+<?php
+/** 
+ * Simple FizzBuzz challenge
+ */
+function fizzbuzz($iterations) {
+    for($i = 1; $i <= $iterations; $i++) {
+        $result = [$i];
+        if ($i % 3 == 0) {
+            $result[] = "fizz";
+        }
+        if ($i % 5 == 0) {
+            $result[] = "buzz";
+        }
+        if (count($result) > 1) {
+            array_shift($result);
+        }
+        echo sprintf("%s <br>", implode(" ", $result));
+    }
+}
